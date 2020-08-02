55274c2ea87d1a844137f785dc200cf974fc453e
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-08-02 08:05:37 -0500

    added values for workout.js

--- models/workout.js
+++ models/workout.js
@@ -1,10 +1,44 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema(
-    {
+const workoutSchema = new Schema({
     //name:.. figure out what values need to be added. date, exercise, name of exercise, duration, weight, reps, sets, distance
-})
+    day: {
+        type: Date,
+        default: Date.now
+    },
+
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Exercise Type is Required"
+        },
+
+        name: {
+            type: String,
+            trim: true,
+            required: "Name of Exercise is Required"
+        },
+
+        duration: {
+            type: Number,
+        },
+
+        weight: {
+            type:Number,
+        },
+
+        reps: {
+            type:Number,
+        },
+
+        sets: {
+            type:Number,
+        }
+     }]
+});
+
 
 const workout = mongoose.model("workout", workoutSchema);
 
--- routes/api-route.js
+++ routes/api-route.js
@@ -1,1 +1,38 @@
 //where the get, get, post calls go.
+
+//create and post
+
+
+//update workout
+app.post("/update/:id", (req, res) => {
+    db.workout.update(
+      {
+        _id: mongojs.ObjectId(req.params.id)
+      },
+      {
+        $set: {
+          //title: req.body.title,
+          //note: req.body.note,
+          //modified: Date.now()
+        }
+      },
+      (error, data) => {
+        if (error) {
+          res.send(error);
+        } else {
+          res.send(data);
+        }
+      }
+    );
+  });
+
+  //view all workouts
+  app.get("/all", (req, res) => {
+    db.workout.find({}, (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.json(data);
+      }
+    });
+  });
